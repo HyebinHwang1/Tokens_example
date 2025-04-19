a29d1852a3defb698cf772eadd6bc45dec653854
HyebinHwang1 <medi.bin@mediquitous.com>
2025-04-19 14:44:26 +0900

    Fix: figma token sync 추가

---
+++ sd.config.js
@@ -0,0 +1,40 @@
+import StyleDictionary from "style-dictionary";
+
+const sd = new StyleDictionary({
+  platforms: {
+    css: {
+      transformGroup: "css",
+      files: [
+        {
+          destination: "./src/app/theme.css",
+          format: "css/variables",
+        },
+      ],
+    },
+  },
+});
+
+await sd.registerFormat({
+  name: "css/variables",
+  format: function (params) {
+    const tokens = params.allTokens;
+    const names = tokens.map((token) => {
+      return { [token.name.replaceAll("core-", "--")]: token["$value"] };
+    });
+
+    const cssVariables = names
+      .map((obj) => {
+        const [key] = Object.keys(obj);
+        return `  ${key}: ${obj[key]};`;
+      })
+      .join("\n");
+
+    return `@theme {\n${cssVariables}\n}`;
+  },
+});
+
+await (
+  await sd.extend({
+    source: ["figma_token.json"],
+  })
+).buildAllPlatforms();
