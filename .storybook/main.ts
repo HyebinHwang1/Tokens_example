be063f9d0fc07345ad73279813c8bc24a1a301c9
HyebinHwang1 <medi.bin@mediquitous.com>
2025-04-23 16:33:16 +0900

    Fix: addon 설정 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,30 @@
 import type { StorybookConfig } from "@storybook/nextjs";
-import { join, dirname } from "path";
+import path from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string) {
-  return dirname(require.resolve(join(value, "package.json")));
-}
+const getAbsolutePath = (packageName: string) => {
+  return path.dirname(require.resolve(path.join(packageName, "package.json")));
+};
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-a11y"),
+  ],
   framework: {
     name: getAbsolutePath("@storybook/nextjs"),
     options: {},
   },
+  docs: {
+    autodocs: "tag",
+  },
 };
+
 export default config;
