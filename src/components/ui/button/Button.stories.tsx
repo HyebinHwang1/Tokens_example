c81c897a8455f111921f2febf4083215b07842a4
HyebinHwang1 <medi.bin@mediquitous.com>
2025-05-02 16:56:23 +0900

    Fix: Example story 추가

--- src/components/ui/button/Button.stories.tsx
+++ src/components/ui/button/Button.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "./Button";
-import { Plus } from "lucide-react";
+import { ArrowRight, HomeIcon, Plus } from "lucide-react";
+import BrandExample from "@/assets/images/brand_example.svg";
+import Image from "next/image";
 
 const meta: Meta<typeof Button> = {
   title: "UI/Button",
@@ -180,6 +182,66 @@ export const Loading: Story = {
       <Button variant="ghost" isLoading>
         Ghost
       </Button>
+    </section>
+  ),
+};
+
+export const Example: Story = {
+  render: () => (
+    <section className="flex gap-8 flex-col">
+      <div className="flex flex-col items-center">
+        <h2 className="mb-2">Card Button(ghost button)</h2>
+        <Button
+          variant="ghost"
+          className="flex items-center gap-2 w-auto h-auto bg-gray-100 p-2 rounded-2xl"
+        >
+          <div className="w-11 h-11 rounded-full relative">
+            <Image
+              src={BrandExample}
+              alt="no image"
+              fill
+              className="rounded-full"
+            />
+          </div>
+          <div className="text-gray-500 flex flex-col items-start">
+            <p className="text-gray-900 text-xs">MARITHÉ FRANÇOIS GIRBAUD</p>
+            <div className="flex gap-2 text-[9px]">
+              <span># マニッシュ</span>
+              <span># マニッシュ</span>
+              <span># マニッシュ</span>
+            </div>
+            <p className="text-[9px]">directed by kazane</p>
+          </div>
+        </Button>
+      </div>
+
+      <div className="flex flex-col items-center">
+        <h2 className="mb-2 flex gap-1">Option Button(default button)</h2>
+        <Button className="w-auto p-2" selected>
+          <ArrowRight className="w-4 h-4" />
+          <span className="text-xs">Color</span>
+          <span className="text-[10px]">(モデル)</span>
+        </Button>
+      </div>
+
+      <div className="flex flex-col items-center">
+        <h2 className="mb-2 flex gap-1">Capsule button(black button)</h2>
+        <Button variant="black" className="rounded-full">
+          button name
+        </Button>
+      </div>
+
+      <div className="flex flex-col items-center">
+        <h2 className="mb-4 flex gap-1">icon button(ghost button)</h2>
+        <Button
+          variant="ghost"
+          className="flex flex-col gap-1 text-gray-900 font-[400]"
+        >
+          <HomeIcon className="w-6 h-6" />
+          <span className="text-[9px]"> タイトル</span>
+          <span className="text-[9px]">home</span>
+        </Button>
+      </div>
     </section>
   ),
 };
