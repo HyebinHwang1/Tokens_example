d9fcf2d2294ac5d13097bef3c5babd19b9e7b6fc
HyebinHwang1 <medi.bin@mediquitous.com>
2025-05-20 17:21:47 +0900

    Fix: button 예제 코드 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
-import { ProductCardExample } from "@/components/ui/card/CardRealExample";
+import { Button } from "@/components/ui/button/Button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen mx-10">
-      <ProductCardExample />
+    <div className="flex flex-col items-center justify-center h-screen mx-10 gap-3">
+      <Button variant="text" size="xs">
+        xsmall text
+      </Button>
+      <Button variant="text" size="sm">
+        small text
+      </Button>
+      <Button variant="text" size="md">
+        medium text
+      </Button>
     </div>
   );
 }
