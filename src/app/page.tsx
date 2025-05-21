df87bbbb837e3541b9463411664a48f76c2618e4
HyebinHwang1 <medi.bin@mediquitous.com>
2025-05-21 17:22:14 +0900

    Fix: input date 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
 "use client";
 
-import { Button } from "@/components/ui/button/Button";
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen mx-10 gap-3">
-      <Button variant="text" size="xs">
-        xsmall text
-      </Button>
-      <Button variant="text" size="sm">
-        small text
-      </Button>
-      <Button variant="text" size="md">
-        medium text
-      </Button>
+    <div className="flex flex-col items-center justify-center h-screen mx-10 gap-3 text-2xs">
+      <label
+        htmlFor="date-input"
+        className="relative w-full border border-gray-300 rounded-md cursor-pointer flex items-center"
+      >
+        <div className="p-2 flex-1">날짜를 선택하세요</div>
+        <input type="date" className="h-full cursor-pointer" id="date-input" />
+      </label>
     </div>
   );
 }
