5619158f2ef6e8d3b724e0d3399c97af57c584bc
HyebinHwang1 <medi.bin@mediquitous.com>
2025-05-23 17:04:26 +0900

    Fix: 예제 페이지 코드 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,28 @@
 "use client";
 
-import { Button } from "@/components/ui/button/Button";
-import {
-  Modal,
-  ModalContent,
-  ModalDescription,
-  ModalFooter,
-  ModalTrigger,
-} from "@/components/ui/modal";
+import { Button } from "@/components/ui/button";
+import { Tooltip } from "@/components/ui/tooltip";
+import { TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center mx-10 gap-3 text-2xs h-screen">
-      <Modal>
-        <ModalTrigger>Open</ModalTrigger>
-        <ModalContent>
-          <ModalDescription>
-            IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。IDまたはパスワvードが一致しません。
-          </ModalDescription>
-          <ModalFooter className="flex flex-row gap-2 border-t border-gray-200">
-            <Button size="full" className="h-10" variant="light">
-              キャンセル
-            </Button>
-            <Button size="full" className="h-10" variant="dark">
-              確認
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button size="lg">Hover</Button>
+          </TooltipTrigger>
+          <TooltipContent
+            side="top"
+            theme="secondary"
+            sideOffset={10}
+            align="start"
+          >
+            <p>Add to library</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </div>
   );
 }
