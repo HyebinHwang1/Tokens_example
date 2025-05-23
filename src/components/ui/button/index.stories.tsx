9bdbb971cf0c7ee1eeb7d4376a566b3cda568b95
HyebinHwang1 <medi.bin@mediquitous.com>
2025-05-23 17:02:16 +0900

    Fix: 버튼 파일명 변경

--- src/components/ui/button/index.stories.tsx
+++ src/components/ui/button/index.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Button } from "./Button";
 import { Plus } from "lucide-react";
+import { Button } from "./index";
 
 export default {
   title: "COMPONENTS/Button",
