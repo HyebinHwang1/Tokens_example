5dbf36ddf7a0f701c03959cced8d08a70ade0fc9
HyebinHwang1 <medi.bin@mediquitous.com>
2025-05-23 14:39:41 +0900

    Fix: 사용하지 않는 커모넌트 삭제 및 파일 위치 이동

--- src/components/ui/text/text.stories.tsx
+++ src/components/ui/text/text.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof Text> = {
-  title: "MODULES/Text",
+  title: "COMPONENTS/Text",
   parameters: {
     layout: "centered",
   },
