c718617ef56b2af71315628e2990381f796ff0d3
HyebinHwang1 <medi.bin@mediquitous.com>
2025-05-27 17:09:05 +0900

    Fix: icona 설정 테스트

---
+++ src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as Cat } from "./Cat";
+export { default as Chicken } from "./Chicken";
+export { default as Dog } from "./Dog";
+export { default as Rabbit } from "./Rabbit";
+export { default as Turtle } from "./Turtle";
