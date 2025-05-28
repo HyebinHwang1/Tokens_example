be69f1e0ff1eea7605626750d4bda2f2e72f53c7
HyebinHwang1 <medi.bin@mediquitous.com>
2025-05-28 13:27:51 +0900

    Fix: 코드레빗 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,15 @@
 import { HomeFilled24 } from "@/components/icons";
 
 export default function Home() {
+  console.log(123123123123);
   return (
     <div className="flex flex-col items-center justify-center mx-10 gap-3 text-2xs h-screen">
+      <Loadingg />
       <HomeFilled24 width={32} height={32} className="text-red-500" />
     </div>
   );
+}
+
+function Loadingg() {
+  return <div>Loading...</div>;
 }
