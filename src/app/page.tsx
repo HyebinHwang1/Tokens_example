7ac221e5b4574e829e447fa09e0685e5d7b0265f
HyebinHwang1 <medi.bin@mediquitous.com>
2025-05-28 10:44:17 +0900

    Fix: Icon export 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { Cat } from "@/components/icons";
+import Cart2Outline from "@/components/icons/Cart2Outline";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center mx-10 gap-3 text-2xs h-screen">
-      <Cat className="w-10 h-10" />
+      <Cart2Outline width={32} height={32} />
     </div>
   );
 }
