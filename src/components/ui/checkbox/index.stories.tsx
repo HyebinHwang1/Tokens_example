44edb5e650b82a86b22345bb3f5dd6433376f558
HyebinHwang1 <medi.bin@mediquitous.com>
2025-06-04 10:43:15 +0900

    Fix: checkbox 수정

--- src/components/ui/checkbox/index.stories.tsx
+++ src/components/ui/checkbox/index.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import React, { useEffect, useState } from "react";
 import { Checkbox } from ".";
 
 const meta: Meta<typeof Checkbox> = {
@@ -6,13 +7,38 @@ const meta: Meta<typeof Checkbox> = {
   component: Checkbox,
   parameters: {
     layout: "centered",
+    docs: {
+      source: {
+        type: "code",
+        language: "tsx",
+      },
+    },
   },
   tags: ["autodocs"],
+  argTypes: {
+    className: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 };
 export default meta;
+
 type Story = StoryObj<typeof Checkbox>;
 
 export const Basic: Story = {
+  argTypes: {
+    checked: {
+      control: "boolean",
+      description: "체크박스의 값을 제어합니다.",
+      name: "checked",
+    },
+    disabled: {
+      control: "boolean",
+      description: "체크박스의 비활성화 상태를 제어합니다.",
+    },
+  },
   args: {
     checked: false,
     disabled: false,
@@ -22,28 +48,36 @@ export const Basic: Story = {
       canvas: {
         sourceState: "shown",
       },
+      description: {
+        story:
+          "가장 기본적인 `Checkbox`의 사용 예시입니다. Storybook 컨트롤을 통해 초기 `checked` 상태와 `disabled` 상태를 제어할 수 있으며, 사용자의 클릭에 따라 상태가 변경됩니다.",
+      },
+      source: {
+        code: `
+  const [isChecked, setIsChecked] = React.useState(false);
+
+<Checkbox
+  checked={isChecked}
+  disabled={disabled}
+  onCheckedChange={() => setIsChecked(!isChecked)}
+/>
+`,
+      },
     },
   },
-  render: (args) => <Checkbox {...args} />,
-};
+  render: function Render(args) {
+    const [isChecked, setIsChecked] = useState(args.checked);
 
-export const Checked: Story = {
-  args: {
-    checked: true,
+    useEffect(() => {
+      setIsChecked(args.checked);
+    }, [args.checked]);
+
+    return (
+      <Checkbox
+        checked={isChecked}
+        disabled={args.disabled}
+        onCheckedChange={() => setIsChecked(!isChecked)}
+      />
+    );
   },
-};
-
-export const Disabled: Story = {
-  args: {
-    disabled: true,
-  },
-};
-
-export const WithLabel: Story = {
-  render: (args) => (
-    <label className="flex items-center gap-2 cursor-pointer">
-      <Checkbox {...args} />
-      <span>이용약관에 동의합니다</span>
-    </label>
-  ),
 };
